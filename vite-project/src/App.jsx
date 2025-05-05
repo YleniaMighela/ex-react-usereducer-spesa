7e5b3d9f03aa6c97258cf8c7a13ecc25ca2906c3
Ylenia <yleniamighela@gmail.com>
2025-05-05 16:39:21 +0200

    milestone 01

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,26 @@
 
 function App() {
 
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
 
   return (
     <>
-      <h1>ciao</h1>
+      <div>
+        <h1>Lista dei prodotti</h1>
+
+        <ul>
+          {products.map((product, index) =>
+          (<li key={index}>
+            <strong>{product.name}:</strong>
+            {product.price} €
+          </li>))}
+        </ul>
+      </div>
     </>
   )
 }
